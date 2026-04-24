245c850d78cd7110ed7951fda150851c71507df1
rajit8674 <rajitmaurya8318@gmail.com>
2026-04-24 23:54:43 +0530

    render

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('CareerForge API is running...');
+});
+
 app.use('/api', apiRoutes);
 
 // Connect to MongoDB
